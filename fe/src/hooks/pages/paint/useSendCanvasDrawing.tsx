844a3c2480af95f2530ca1fee0c5ba9c3581c219
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2023-05-22 19:55:39 +0900

    canvas에 이벤트 연결하는 부분 hooks 분리

---
+++ fe/src/hooks/pages/paint/useSendCanvasDrawing.tsx
@@ -0,0 +1,56 @@
+import { useEffect } from "react";
+
+interface UseSendCanvasDrawingParams {
+  drawingDataChannel?: RTCDataChannel;
+  color?: string;
+  lineWidth?: number;
+}
+
+export default function useSendCanvasDrawing(props: UseSendCanvasDrawingParams) {
+  const { drawingDataChannel, color = "#000000", lineWidth = 2.5 } = props;
+
+  useEffect(() => {
+    if (!drawingDataChannel) return;
+
+    const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
+
+    canvas.width = canvas.getBoundingClientRect().width;
+    canvas.height = canvas.getBoundingClientRect().height;
+
+    const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+    context.strokeStyle = color;
+    context.lineWidth = lineWidth;
+
+    let isPainting = false;
+
+    const startDrawing = () => {
+      isPainting = true;
+    };
+    const stopDrawing = () => {
+      isPainting = false;
+    };
+
+    canvas.addEventListener("mousedown", startDrawing);
+    canvas.addEventListener("mouseup", stopDrawing);
+    canvas.addEventListener("mouseleave", stopDrawing);
+
+    canvas.addEventListener("pointermove", (event) => {
+      const x = event.offsetX;
+      const y = event.offsetY;
+
+      if (isPainting) {
+        context.lineTo(x, y);
+        context.stroke();
+      } else {
+        context.beginPath();
+        context.moveTo(x, y);
+      }
+
+      const relativeX = x / canvas.getBoundingClientRect().width;
+      const relativeY = y / canvas.getBoundingClientRect().height;
+
+      const data = { isPainting, relativeX, relativeY };
+      drawingDataChannel.send(JSON.stringify(data));
+    });
+  }, [drawingDataChannel]);
+}
